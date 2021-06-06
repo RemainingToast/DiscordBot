61d2fefe7d31f20b51ad92dc9e196e800416331a
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-06-06 11:10:59 +0200

    Add invite repost feature

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const path = require('path')
 
 const config = require('./config.json')
 const axios = require('axios')
+const {TextChannel} = require("discord.js");
 
 const client = new commando.CommandoClient({
     owner: config.owner_id,
     commandPrefix: config.prefix
 })
 
+var inviteCache = [];
+
 client.on('ready', () => {
     console.log('[INFO] Bot is ready and starting!')
 
+    // @ts-ignore
     client.registry
         .registerGroups([
             ['admin', 'admin commands'],
@@ -23,20 +27,37 @@ client.on('ready', () => {
 
     console.log('[INFO] Commands registered')
 
+    console.log('[INFO] Scheduling custom status')
     setInterval(() => {
         axios.get(`https://mcapi.xdefcon.com/server/${config.server_ip}/full/json`).then((result) => {
             const status = String(result.data.serverStatus)
             const players = String(result.data.players)
 
-            if(status.startsWith('offline') || status === 'offline'){
-                client.user.setPresence({ activity: { name: `Server is offline :(`, type: 0 }, status: 'dnd' })
+            if (status.startsWith('offline') || status === 'offline') {
+                client.user.setPresence({activity: {name: `Server is offline :(`, type: 0}, status: 'dnd'})
             } else {
-                client.user.setPresence({ activity: { name: `${players} players`, type: "WATCHING" }, status: 'online' })
+                client.user.setPresence({activity: {name: `${players} players`, type: "WATCHING"}, status: 'online'})
             }
 
-        }).catch((error) => { console.log(error) })
+        }).catch((error) => {
+            console.log(error)
+        })
     }, 5000)
+})
 
+client.on('message', message => {
+    if (message.author === client.user) return
+
+    if (message.channel instanceof TextChannel) {
+        if (message.channel.id === config.ad_channel_id) {
+            const invite = message.content.match(/(https?:\/\/)?(www.)?(discord.(gg|io|me|li)|discordapp.com\/invite)\/[^\s/]+?(?=\b)/g)
+            message.delete()
+            if (invite != null && !inviteCache.includes(invite[0])) {
+                message.channel.send(invite[0])
+                inviteCache.push(invite[0])
+            }
+        }
+    }
 })
 
 client.login(config.token)
